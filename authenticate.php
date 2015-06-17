d481f7b99694944f325d24c17d50ba47f5fcfa60
nielubowicz <nielubowicz@gmail.com>
2015-06-16 23:31:01 -0400

    Adds send-sms-signup function that sends an invitation to members

--- authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$string = file_get_contents("api.keys.live");
-$json_a = json_decode($string, true);
-
-$AccountSid = $json_a['ACCOUNT SID'];
-$AuthToken = $json_a['AUTH TOKEN'];
-
-?>
--- send-sms-signup.php
+++ send-sms-signup.php
@@ -1,28 +1,39 @@
 <?php
 require "../../php/twilio-php-master/Services/Twilio.php";
-require_once "authenticate.php";
- 
-global $AccountSid;
-global $AuthToken;
-$phoneNumberFile = "numbers.live";
-$client = new Services_Twilio($AccountSid, $AuthToken);
+require_once "config.php";
 
-$string = file_get_contents($phoneNumberFile);
-$json_a = json_decode($string, true);
+    $FromNumber = $TWILIO_JEA_CONFIG['OutgoingNumber'];
+    $client = new Services_Twilio($TWILIO_JEA_CONFIG['AccountSid'], $TWILIO_JEA_CONFIG['AuthToken']);
+    $questionString = "Epilepsy continues to be a misunderstood, stigmatizing condition and it does not have to be! Join EpiCenter at JEA to help End the Stigma. Text SIGNUP to join!";
 
-$FromNumber = $json_a['from'];
-$ToNumber = $json_a['to'];
+    // Create connection
+    $sql_connection = new mysqli($SQL_JEA_CONFIG['servername'], $SQL_JEA_CONFIG['username'], $SQL_JEA_CONFIG['password'], $SQL_JEA_CONFIG['db']);
+    // Check connection
+    if ($sql_connection->connect_error) {
+        die("Connection failed: " . $sql_connection->connect_error);
+    } 
 
-$questionString = "Epilepsy continues to be a misunderstood, stigmatizing condition and it does not have to be! Join EpiCenter at JEA to help End the Stigma. Text SIGNUP to join!";
+    $sql = "SELECT * FROM signups WHERE Status=0"; // select interested, uncontacted people
+    $result = $sql_connection->query($sql);
 
-foreach ($ToNumber as $recipientPhoneNumber) { 
-    $result = mysql_query($sql);
-    $message = $client->account->messages->sendMessage(
-      $FromNumber, // From a valid Twilio number
-      $ToNumber, // Text this number
-      $questionString
-    );
-}
+    $ToNumbers = array();
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $ToNumber = $row['PhoneNumber'];
+            array_push($ToNumbers, "'".$ToNumber."'");
+            $message = $client->account->messages->sendMessage(
+              $FromNumber, // From a valid Twilio number
+              $ToNumber, // Text this number
+              $questionString
+            );
+        }
+    } else {
+        echo "No numbers found to send signup message\n";
+    }
 
+    $sql = "UPDATE signups SET Status=1 WHERE PhoneNumber IN (" . implode(",", $ToNumbers) . ");";
+    $sql_connection->query($sql);
+    $sql_connection->close();
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
